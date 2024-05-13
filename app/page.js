4a63e7415993ea38420657df1f9b46866dce1d23
Seb Gualtero <JuanSebastian.GualteroVillamil@edu.sait.ca>
2024-05-13 17:59:37 -0600

    Assignment Week 2

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
-      <p>Welcome to the home page</p>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2">Week 2</Link>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Juan Sebastian Gualtero</p>
+      <Link href="https://github.com/sebgualtero/cprg306-assignments">
+        GitHub Repository
+      </Link>
+    </div>
+  );
+}
